1e9c9a25c14463701462039fefcb808c4a906761
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-21 18:28:25 -0300

    feat: update user router to use Customer schema for user creation

--- back-end/src/routers/index.ts
+++ back-end/src/routers/index.ts
@@ -1,5 +1,2 @@
 export * from './users-router';
 export * from './authentication-router';
-export * from './transaction-router';
-export * from './credit-router';
-export * from './debt-router';
--- back-end/src/routers/users-router.ts
+++ back-end/src/routers/users-router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import { createUserSchema } from '@/schemas';
+import { createCustomerSchema } from '@/schemas';
 import { validateBody } from '@/middlewares';
 import { usersPost } from '@/controllers';
 
 const usersRouter = Router();
 
-usersRouter.post('/', validateBody(createUserSchema), usersPost);
+usersRouter.post('/', validateBody(createCustomerSchema), usersPost);
 
 export { usersRouter };
