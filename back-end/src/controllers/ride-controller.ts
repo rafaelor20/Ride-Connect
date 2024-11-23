dada7c99b34b402920b0d32c142a0c626c791e9a
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-23 17:32:43 -0300

    refactor: remove unused Request import in ride-controller

--- back-end/src/controllers/ride-controller.ts
+++ back-end/src/controllers/ride-controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import rideService from '@/services/ride-service';
