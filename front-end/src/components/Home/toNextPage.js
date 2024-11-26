064f4941af6f297f28bda1448defc677de724f21
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-26 02:47:51 -0300

    refactor: replace Content div with Button component in ToNextPage for improved accessibility

--- front-end/src/components/Home/toNextPage.js
+++ front-end/src/components/Home/toNextPage.js
@@ -1,31 +1,14 @@
-import styled from 'styled-components';
 import Link from '../Link';
+import Button from '../../components/Form/Button';
 
 export default function ToNextPage(props) {
   return (
     <Link to={props.nextPage}>
-      <Content>
+      <Button>
         <p>
           {props.description}
         </p>
-      </Content>
+      </Button>
     </Link>
   );
 }
-
-const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 80%;
-    min-width: 180px;
-    height: 40%;
-    min-height: 50px;
-    background-color: black;
-    p{
-        font-size: 18px;
-        font-weight: bold;
-        color: white;
-    }
-`;
