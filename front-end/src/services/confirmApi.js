8ebc1ba60f1d73ddcca9df350c487653119ca54e
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-27 00:41:38 -0300

    fix: remove token parameter from confirm and getRides functions for simplified API calls

--- front-end/src/services/confirmApi.js
+++ front-end/src/services/confirmApi.js
@@ -1,6 +1,6 @@
 import api from './api';
 
-export async function confirm( body, token) {
+export async function confirm( body) {
   const response = await api.patch('/ride/confirm', body);
   return response.data;
 }
--- front-end/src/services/getRides.js
+++ front-end/src/services/getRides.js
@@ -1,13 +1,9 @@
 import api from './api';
 
-export async function getRides(body, token) {
+export async function getRides(body) {
   let { driver_id } = body;
   const url = driver_id ? `/ride/?driver_id=${driver_id}` : '/ride';
   
-  const response = await api.get(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await api.get(url);
   return response.data;
 }
