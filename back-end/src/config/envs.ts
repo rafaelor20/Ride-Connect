fdbc8e303d1ac0c68d9ba96226c5139d10d50515
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-26 22:23:31 -0300

    chore: consolidate environment variable files and update paths for configuration

--- back-end/src/config/envs.ts
+++ back-end/src/config/envs.ts
@@ -2,15 +2,7 @@ import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 
 export function loadEnv() {
-  let path = '.env'; // default path
-
-  if (process.env.NODE_ENV === 'test') {
-    path = '.env.test';
-  } else if (process.env.NODE_ENV === 'development') {
-    path = '.env.development';
-  } else if (process.env.NODE_ENV === 'local') {
-    path = '.env.local';
-  }
+  const path = '../.env'; // default path
 
   const currentEnvs = dotenv.config({ path });
   dotenvExpand.expand(currentEnvs);
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,7 +3,7 @@ import app, { init } from '@/app';
 
 dotenv.config();
 
-const port = +process.env.PORT || 5000;
+const port = +process.env.PORT;
 
 init().then(() => {
   app.listen(port, () => {
