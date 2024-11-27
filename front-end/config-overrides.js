8daf0a95e8019032a63ef5cf1fd71410eaf97469
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-27 14:11:31 -0300

    fix: update seed:start script for initial migration and seeding; remove obsolete migration files and adjust dotenv path

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -4,7 +4,7 @@ module.exports = function override(config, env) {
   config.plugins = [
     ...config.plugins,
     new Dotenv({
-      path: '../.env', // Path to your .env file (parent directory)
+      path: '.env', // Path to your .env file (parent directory)
       systemvars: true, // Load system environment variables as well
     }),
   ];
