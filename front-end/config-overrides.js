faefecec914ddb4a74ace4833a918bafb5156269
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-27 02:55:59 -0300

    fix: integrate dotenv-webpack for environment variable management and update scripts for react-app-rewired

---
+++ front-end/config-overrides.js
@@ -0,0 +1,13 @@
+const Dotenv = require('dotenv-webpack');
+
+module.exports = function override(config, env) {
+  config.plugins = [
+    ...config.plugins,
+    new Dotenv({
+      path: '../.env', // Path to your .env file (parent directory)
+      systemvars: true, // Load system environment variables as well
+    }),
+  ];
+
+  return config;
+};
