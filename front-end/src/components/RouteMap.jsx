243bc282c36819c7508dd2e7dddc37a18e6b5787
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2025-06-02 15:46:59 -0300

    fix: update Google Maps API key environment variable to use Vite

--- front-end/src/components/RouteMap.jsx
+++ front-end/src/components/RouteMap.jsx
@@ -30,7 +30,7 @@ const RouteMap = (props) => {
 
   return (
     <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+      googleMapsApiKey={import.meta.env.VITE_API_GOOGLE_API_KEY}
       onLoad={() => setMapLoaded(true)}
     >
       <GoogleMap
